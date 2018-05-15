91245ddb264f4bddeefb9899c47ebe7cb4940ff1
nfagan <nick@fagan.org>
2018-05-15 12:28:45 -0400

    update drug labels

--- +hwwa/add_drug_labels.m
+++ +hwwa/add_drug_labels.m
@@ -4,18 +4,24 @@ import shared_utils.assertions.*;
 
 assert__isa( labs, 'fcat', 'labels' );
 
-saline_days = { '0215', '0221', '0228', '0302', '0303', '0305' };
-serotonin_days = { '0214', '0216', '0220', '0222', '0301', '0304' };
+saline_days = { '0215', '0221', '0228', '0302', '0303', '0305' ...
+  , '0508', '0510' };
+serotonin_days = { '0214', '0216', '0220', '0222', '0301', '0304' ...
+  , '0509', '0511', '0514' };
+none_days = { '0412', '0410' };
 
-for i = 1:numel(saline_days)
-  ind = find( labs, [ saline_days{i}, '.mat' ] );
-  if ( isempty(ind) ), continue; end
-  labs(ind, 'drug') = 'saline';
+add_labs( labs, saline_days, 'saline' );
+add_labs( labs, serotonin_days, '5-htp' );
+add_labs( labs, none_days, 'no drug' );
+
 end
-for i = 1:numel(serotonin_days)
-  ind = find( labs, [ serotonin_days{i}, '.mat' ] );
+
+function add_labs(labs, days, drug)
+
+for i = 1:numel(days)
+  ind = find( labs, [ days{i}, '.mat' ] );
   if ( isempty(ind) ), continue; end
-  labs(ind, 'drug') = '5-htp';
+  setcat( labs, 'drug', drug, ind );
 end
 
 end
--- script/pipeline.m
+++ script/pipeline.m
@@ -1,6 +1,6 @@
 %%  pipeline
 
-inputs = { 'overwrite', false, 'files_containing', '041' };
+inputs = { 'overwrite', false, 'files_containing', '05' };
 
 %%  save unified trial data
 
