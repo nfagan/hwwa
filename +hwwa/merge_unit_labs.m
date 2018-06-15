7014059ee96e51da27c75a6caf821b2ee72eac5b
nfagan <nick@fagan.org>
2018-06-15 10:56:04 -0400

    remove commented out code

--- +hwwa/merge_unit_labs.m
+++ +hwwa/merge_unit_labs.m
@@ -17,20 +17,3 @@ repmat( data_labs, n_units );
 prune( merge(data_labs, psth_labs) );
 
 end
-
-% psth_cats = getcats( psth_labs );
-% addcat( data_labs, psth_cats );
-% repmat( data_labs, n_units );
-% 
-% stp = 1;
-% 
-% for i = 1:n_units
-%   ind = stp:stp+data_sz-1;
-%   for j = 1:numel(psth_cats)
-%     c = psth_cats{j};
-%     setcat( data_labs, c, partcat(psth_labs, c, ind), ind );
-%   end
-%   stp = stp + data_sz;
-% end
-% 
-% prune( data_labs );
